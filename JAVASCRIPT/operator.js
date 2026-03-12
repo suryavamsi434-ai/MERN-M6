a023a4f0ae9c3a20b2a8cdc19f265006f26d90a6
suryavamsi434-ai <suryavamsi434@gmail.com>
2026-03-12 12:22:48 +0530

    Add files via upload

---
+++ JAVASCRIPT/operator.js
@@ -0,0 +1,49 @@
+/*
+let x=22;
+let y=7;
+let z=x+y;
+console.log(z);
+*/
+/*
+let x=22;
+let y=5;
+console.log(x+y);
+console.log(x-y);
+console.log(x*y);
+console.log(x/y);
+
+*/
+/*
+//forloop
+for(let i=0;i<9;i++){
+console.log(i);
+
+}
+*/
+/*
+let x=29;
+if(x>10){
+    console.log("x is greater than 10");
+    
+}
+else{
+    console.log("x is less than 10");
+}
+    */
+
+//else if
+
+let aadhaar=false;
+let pan=true;
+if(aadhaar){
+    console.log("you can open account in our bank--using aadhaar");
+    
+}
+else if(pan){
+ console.log("you can open account in our bank--using pan");
+ 
+}
+else{
+     console.log("you can not open account in our bank");
+ 
+}
---
+++ JAVASCRIPT/surya.js
@@ -0,0 +1,8 @@
+let age=18;
+let d1=  false;
+if(age>18 || d1 == true ) {
+    console.log("you can eligible for voting");
+} else {
+    console.log("you can not eligible for voting");
+
+}
