7ab9aaa93e9ba9353a9d584c8fbb3145eea195f8
suryavamsi434-ai <suryavamsi434@gmail.com>
2026-03-13 11:59:25 +0530

    Add files via upload

---
+++ surya.js
@@ -0,0 +1,125 @@
+/*
+const person= {
+    name:"surya",
+    age:22,
+    greet:function(){
+        return`Hello,my name is ${this.name} ;
+        
+    }
+
+}
+console.log(person.name);
+console.log(person.greet());
+
+*/
+    
+    
+/*
+const person={Name:"surya",age:22,city:"indore"};
+const{Name,age,city}=person;
+console.log(Name);
+console.log(age);
+console.log(city);
+
+*/
+
+/*
+let n=15;
+for(let i=1;i<=10;i++){
+    console.log(`${n} x ${i}=`,n*i);
+    
+}
+    */
+
+/*
+let fruits=['apple','banana','orange'];
+let phones=['app.le','samsung','oneplus'];
+let score=[];
+
+*/
+/*
+let fruits=['apple','banana','orange','mango'];
+console.log(fruits[0]);
+console.log(fruits[2]);
+console.log(fruits[3]);
+console.log(fruits[2-1]);
+
+*/
+/*
+
+//array push
+let arr=[1,2,3,4];
+arr.push(5,7,8,9);
+console.log(arr);
+*/
+
+/*
+let arr=[1,2,3,4];
+arr.pop();
+arr.push(5);
+console.log(arr);
+*/
+/*
+//shift
+
+
+let arr=[1,2,3,4];
+arr.shift();
+console.log(arr);
+
+//unshift
+
+
+let arr=[1,2,3,4];
+arr.unshift();
+console.log(arr);
+*/
+/*
+// array length
+let arr=[1,2,3,4];
+console.log(arr.length);
+*/
+
+
+
+
+/*
+
+//array push
+let arr=[1,2,3,4];
+arr.push(5,7,8,9);
+console.log(arr);
+
+
+//pop
+arr.pop();
+arr.push(5);
+console.log(arr);
+
+
+//shift
+
+
+arr.shift();
+console.log(arr);
+
+//unshift
+
+
+
+arr.unshift();
+console.log(arr);
+
+
+// array length
+
+console.log(arr.length);
+*/
+
+let str1= "Hello,world";
+let str2='hello,javascript';
+let str3=`hello,universe`;
+console.log(str1);
+console.log(str2);
+console.log(str3);
+
